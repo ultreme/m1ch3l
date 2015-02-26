e8f5916e75aab7758066b77f379bc755bc2a84e6
s. rannou <mxs@sbrk.org>
2015-02-26 13:45:00 +0100

    Added script to pull new commands from git. #1

---
+++ misc/pull-commands.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd
+
+if ! [ -d m1ch3l ]
+then
+    git clone git@github.com:ultreme/m1ch3l.git
+fi
+
+( cd m1ch3l && git pull)
+
+cp ~/m1ch3l/irc/public/* /home/m1ch3l/gorobot/root/scripts/public/
