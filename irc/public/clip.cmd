a45c6b8b626adf6a94b9debd8910a9c1b812c066
Cocolasticot <CocolasticotSCC@users.noreply.github.com>
2015-02-28 12:56:45 +0100

    Ajout de clip.cmd
    !clip donne l'URL de la recherche youtube pour le couple artiste/titre en cours

---
+++ irc/public/clip.cmd
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+po=$1
+se=$2
+ch=$3
+us=$4
+
+google="https://www.youtube.com/results?search_query="
+artist=$(curl -s "http://62.210.180.173:12348/?action=infos&format=json" | python -mjson.tool | grep artist | tail -n 1 | awk -F : '{print $2}' | tr -d ',' | tr -d '"')
+title=$(curl -s "http://62.210.180.173:12348/?action=infos&format=json" | python -mjson.tool  | grep left_title | tail -n 1 | awk -F : '{print $2}' | tr -d ',' | tr -d '"')
+
+music=$(echo $artist$title | tr ' ' '%20')
+url=$google$music
+
+echo "$se 1 PRIVMSG $ch :${us}> $url" | nc -q 1 localhost $po
