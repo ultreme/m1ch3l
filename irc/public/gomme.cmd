972b8f454c9f786db508d143b5304a0ca35008da
m1ch3l <ultreme@sbrk.org>
2015-03-02 23:15:04 +0100

    gomme gomme une gomme

--- irc/public/gomme.cmd
+++ irc/public/gomme.cmd
@@ -4,4 +4,4 @@ se=$2
 ch=$3
 us=$4
 
-echo "$se 1 PRIVMSG $ch :${us}> https://soundcloud.com/camembert-au-lait-crew/une-gomme" | nc -q 1 localhost $po
+echo "$se 1 PRIVMSG $ch :${us}> gomme" | nc -q 1 localhost $po
