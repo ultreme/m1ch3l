15fe1ccd1de3400c8b176f748771a65e988a18d6
s. rannou <mxs@sbrk.org>
2015-03-02 21:11:47 +0100

    Added PHP example. fixes #6

---
+++ irc/public/garcon.cmd
@@ -0,0 +1,45 @@
+#!/usr/bin/env php
+<?php
+
+$port = $argv[1];
+$server	= $argv[2];
+$channel = $argv[3];
+$user = $argv[4];
+$priority = 1;
+$params = array();
+
+for ($i = 5; $i < sizeof($argv); ++$i)
+  {
+    $params[$i - 5] = $argv[$i];
+  }
+
+$sock = fsockopen("localhost", $port);
+
+if (!$sock)
+  {
+    die(sprintf("can't connect to localhost %d\n", $port));
+  }
+
+function	send_message($str)
+{
+  global	$sock, $priority, $channel, $server, $user;
+  fwrite($sock, sprintf("%s %d PRIVMSG %s :%s -> %s\n", $server, $priority, $channel, $user, $str));
+}
+
+function	grand_garcon()
+{
+  global	$params;
+
+  if (!count($params))
+  {
+	send_message("t'es pas trop un grand garçon");
+  }
+
+  foreach ($params as $garcon)
+  {
+	send_message("t'es un vrai petit garçon " . $garcon);
+  }
+}
+
+grand_garcon($argv);
+fclose($sock);
