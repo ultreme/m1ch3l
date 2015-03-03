e7d379b9ce05c4c21417fdf80de74d50d81b508f
m1ch3l <ultreme@sbrk.org>
2015-03-03 22:55:04 +0100

    gomme gomme une gomme

--- irc/public/clip.cmd
+++ irc/public/clip.cmd
@@ -6,14 +6,10 @@ ch=$3
 us=$4
 
 google="https://www.youtube.com/results?search_query="
-full_title=$(curl -s "http://62.210.180.173:12348/?action=infos&format=json" | python -mjson.tool | grep "full_title"  | head -n 1 | awk -F : '{print $2}' | tr -d ',' | tr -d '"')
+artist=$(curl -s "http://62.210.180.173:12348/?action=infos&format=json" | python -mjson.tool | grep "artist" | tail -n 1 | awk -F : '{print $2}' | tr -d ',' | tr -d '"')
+title=$(curl -s "http://62.210.180.173:12348/?action=infos&format=json" | python -mjson.tool  | grep "left_title" | tail -n 1 | awk -F : '{print $2}' | tr -d ',' | tr -d '"')
 
-if [ "$full_title"="" ]
-then
-  full_title=$(curl -s "http://62.210.180.173:12348/?action=infos&format=json" | python -mjson.tool | grep "filename"  | head -n 1 | awk -F / '{print $5}' | tr -d ',' | tr -d '"')
-fi
-
-music=$(echo $full_title | sed 's/\ /%20/g')
+music=$(echo $artist$title | sed 's/\ /%20/g')
 url=$google$music
 
 echo "$se 1 PRIVMSG $ch :${us}> $url" | nc -q 1 localhost $po
