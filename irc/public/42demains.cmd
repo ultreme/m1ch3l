6d51485c1d587be47f6ba3e65e64e669bca881c7
Manfred Touron <m@42.am>
2015-12-22 18:33:02 +0100

    Update 42demains.cmd

--- irc/public/42demains.cmd
+++ irc/public/42demains.cmd
@@ -4,4 +4,5 @@ se=$2
 ch=$3
 us=$4
 
-echo "$se 1 PRIVMSG $ch :${us}> dans 42 demains: $(LANG=fr_FR date -v+42d)" | nc -q 1 localhost $po
+futur=$(date -v+42d)
+echo "$se 1 PRIVMSG $ch :${us}> dans 42 demains: $futur" | nc -q 1 localhost $po
