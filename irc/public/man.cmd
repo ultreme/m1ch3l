34d66901c20c377aa0a48c14ceeac62ad1acd165
Manfred Touron <m@42.am>
2016-03-01 17:03:54 +0100

    Update man.cmd

--- irc/public/man.cmd
+++ irc/public/man.cmd
@@ -5,6 +5,6 @@ se=$2
 ch=$3
 us=$4
 
-text=$(printf "superman\nbatman\nspiderman\npoliceman\nmanfred\nsupermanfred\nspidermanfred\npolicemanfred\nbatmanfred" | sort -R | head -1)
+text=$(printf "superman\nbatman\nspiderman\npoliceman\nmanfred\nsupermanfred\nspidermanfred\npolicemanfred\nbatmanfred\nironman\nironmanfred\npowerman\nircman\ncoolman\nsympaman\nbienman\nplutot-cool-man\nplutot-sympa-man\nmuscleman\njambonman\nviandeman\nharicotman\nshaman\nvadiman\nmartinman\njamesman\nlouisman\nmaman" | sort -R | head -1)
 
 echo "$se 1 PRIVMSG $ch :${us}> $text" | nc -q 1 localhost $po
