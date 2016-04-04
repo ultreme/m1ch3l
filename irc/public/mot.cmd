8d594d1ac28173b519120feaa9ff3174f71d7d8d
Manfred Touron <m@42.am>
2016-04-04 23:42:27 +0200

    c'est relou à la longue :)

--- irc/public/mot.cmd
+++ irc/public/mot.cmd
@@ -4,4 +4,4 @@ se=$2
 ch=$3
 us=$4
 
-echo "$se 1 PRIVMSG $ch :${us}> \[MOT\] Essayez de retrouver le mot originel avec ces lettres : b j a e G" | nc -q 1 localhost $po
+#echo "$se 1 PRIVMSG $ch :${us}> \[MOT\] Essayez de retrouver le mot originel avec ces lettres : b j a e G" | nc -q 1 localhost $po
